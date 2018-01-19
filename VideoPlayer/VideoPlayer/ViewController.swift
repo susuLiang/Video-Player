72058a0b623f2daffa5112e0a07064f99338fa62
Susu Liang <sophie790709@gmail.com>
2018-01-19 15:22:33 +0800

    Can pause、play、mute

--- VideoPlayer/VideoPlayer/ViewController.swift
+++ VideoPlayer/VideoPlayer/ViewController.swift
@@ -10,13 +10,22 @@ import UIKit
 import AVKit
 import AVFoundation
 
+class Player: NSObject {
+
+    let player: AVPlayer = AVPlayer()
+
+    @objc dynamic var timeControlStatus: AVPlayerTimeControlStatus = .waitingToPlayAtSpecifiedRate
+
+}
+
 class ViewController: UIViewController {
-    
+
 //    let videoURL = URL(string: "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8")
     var videoURL: URL?
-    var player = AVPlayer()
+    var player = Player()
     var playerLayer: AVPlayerLayer!
-    
+    var isMuted = false
+
     var urlTextField: UITextField = {
         let textField = UITextField()
         textField.frame = CGRect(x: 10, y: 44, width: UIScreen.main.bounds.width - 20, height: 30)
@@ -24,35 +33,88 @@ class ViewController: UIViewController {
         return textField
     }()
 
+    var playOrPuaseButton: UIButton = {
+        let button = UIButton()
+        button.frame = CGRect(x: 10, y: UIScreen.main.bounds.height - 100, width: 100, height: 50)
+        button.backgroundColor = .white
+        button.setTitle("Pause", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(pause), for: .touchUpInside)
+        return button
+    }()
+
+    var muteButton: UIButton = {
+        let button = UIButton()
+        button.frame = CGRect(x: UIScreen.main.bounds.width - 110, y: UIScreen.main.bounds.height - 100, width: 100, height: 50)
+        button.backgroundColor = .white
+        button.setTitle("Mute", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(mute), for: .touchUpInside)
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
-        
+
         urlTextField.delegate = self
-        
-        playerLayer = AVPlayerLayer(player: player)
+
+        playerLayer = AVPlayerLayer(player: player.player)
         playerLayer.frame = self.view.bounds
         self.view.layer.insertSublayer(playerLayer, at: 1)
+
         self.view.addSubview(urlTextField)
+        self.view.addSubview(playOrPuaseButton)
+        self.view.addSubview(muteButton)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
+        print(keyPath)
+    }
+
+    @objc func pause(_ sender: UIButton) {
+        player.player.pause()
+        sender.setTitle("Pause", for: .normal)
+        player.timeControlStatus = .paused
+        sender.removeTarget(nil, action: nil, for: .allEvents)
+        sender.addTarget(self, action: #selector(play), for: .touchUpInside)
+    }
+
+    @objc func play(_ sender: UIButton) {
+        player.player.play()
+        sender.setTitle("Play", for: .normal)
+        player.timeControlStatus = .playing
+        sender.removeTarget(nil, action: nil, for: .allEvents)
+        sender.addTarget(self, action: #selector(pause), for: .touchUpInside)
+    }
+
+    @objc func mute(_ sender: UIButton) {
+        self.isMuted = !isMuted
+        player.player.isMuted = isMuted
+    }
 
 }
 
 extension ViewController: UITextFieldDelegate {
-    
+
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         videoURL = URL(string: textField.text!)
         let playerItem = AVPlayerItem(url: videoURL!)
-        player.replaceCurrentItem(with: playerItem)
-        player.play()
+        let observation = player.observe(\Player.timeControlStatus, changeHandler: { (new, old) in
+            print(new)
+            print(old)
+
+        })
+//        player.addObserver(self, forKeyPath:  , options: [ .new, .old], context: nil)
+        player.player.replaceCurrentItem(with: playerItem)
+
+        player.player.play()
         return true
     }
-    
-}
 
+}
