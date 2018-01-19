bfaeca36de30e6709feea09c04107a17a4dfcdf4
Susu Liang <sophie790709@gmail.com>
2018-01-19 13:15:55 +0800

    Create repo

---
+++ VideoPlayer/VideoPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Susu Liang on 2018/1/19.
+//  Copyright © 2018年 Susu Liang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
